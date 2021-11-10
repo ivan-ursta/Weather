8f7018f667220135471dcf8e68aaae69ef6fe0c5
ivan-ursta <65234223+ivan-ursta@users.noreply.github.com>
2021-11-10 19:36:57 +0200

    Added other weather

--- weatherAPI.js
+++ weatherAPI.js
@@ -1,5 +1,6 @@
-let geoLoc = navigator.geolocation;
+import toCompass from "./degToCompass.js";
 
+let geoLoc = navigator.geolocation;
 let watchID;
 
 function errorHandler(err) {
@@ -23,6 +24,7 @@ function showPosition(position) {
     let longitude = position.coords.longitude;
     console.log("Latitude : " + latitude + " Longitude: " + longitude);
     openRequest(latitude, longitude);
+    openRequest1(latitude, longitude);
 }
 
 function openRequest(lat, lon) {
@@ -34,60 +36,149 @@ function openRequest(lat, lon) {
     }
 
     let weatherObj;
-    let weatherDiv = $(".weather");
-    let infoDiv = $(".info");
-    let iconDiv = $(".icon");
-    let tempDiv = $(".temp");
-    let dayTimeDiv = $(".dayTime");
+    let currentInfoDiv = $(".currentInfo");
+    let currentIconDiv = $(".currentIcon");
+    let currentTempDiv = $(".currentTemp");
+    let currentDayTimeDiv = $(".currentDayTime");
 
-    request.open("GET", `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,daily,alerts&units=metric&APPID=a00b1af3da62c683cbdd2b93bb19dc93`);
+    request.open("GET", `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&cnt=2&exclude=minutely,daily,alerts&units=metric&APPID=a00b1af3da62c683cbdd2b93bb19dc93`);
+
     request.onload = function() {
         if (request.status === 200) {
             weatherObj = JSON.parse(request.response);
             console.log(weatherObj);
 
-            let wDate = weatherObj.current.dt;
-            let wIcon = weatherObj.current.weather[0].icon;
-            let wDescription = weatherObj.current.weather[0].description;
-            let wTemp = weatherObj.current.temp;
-            let wFeels = weatherObj.current.feels_like;
-            let wSunrise = weatherObj.current.sunrise;
-            let wSunset = weatherObj.current.sunset;
+            let currentDate = weatherObj.current.dt;
+            let currentIcon = weatherObj.current.weather[0].icon;
+            let currentDescription = weatherObj.current.weather[0].description;
+            let currentTemp = weatherObj.current.temp;
+            let currentFeels = weatherObj.current.feels_like;
+            let currentSunrise = weatherObj.current.sunrise;
+            let currentSunset = weatherObj.current.sunset;
+
+            let currentWeatherDate = new Date(1000 * currentDate);
+            let currentWeatherDay = String(currentWeatherDate.getDate());
+            let currentWeatherMonth = String(currentWeatherDate.getMonth() + 1); //January is 0!
+            let currentWeatherYear = currentWeatherDate.getFullYear();
+            let currentDateElem = $("<h4></h4>").text(currentWeatherDay + '.' + currentWeatherMonth + '.' + currentWeatherYear);
+
+            let currentIconElem = $(`<img src = "https://openweathermap.org/img/wn/${currentIcon}.png"></img>`);
+            let currentDescriptionElem = $("<p></p>").text(currentDescription);
+            let currentTempElem = $("<h1></h1>").html(currentTemp + '&deg;' + 'C');
+            let currentFeelsElem = $("<p></p>").html("Real Feel " + currentFeels + '&deg;');
+
+            let currentSunriseDate = new Date(1000 * currentSunrise);
+            let currentSunriseHours = String(currentSunriseDate.getHours());
+            let currentSunriseMinutes = String(currentSunriseDate.getMinutes());
+            let currentSunriseElem = $("<p></p>").text("Sunrise: " + currentSunriseHours + ":" + currentSunriseMinutes);
+
+            let currentSunsetDate = new Date(1000 * currentSunset);
+            let currentSunsetHours = String(currentSunsetDate.getHours());
+            let currentSunsetMinutes = String(currentSunsetDate.getMinutes());
+            let currentSunsetElem = $("<p></p>").text("Sunset: " + currentSunsetHours + ":" + currentSunsetMinutes);
+
+            let currentDurationHours = currentSunsetHours - currentSunriseHours;
+            let currentDurationMinutes = currentSunsetMinutes - currentSunriseMinutes;
+            let currentDurationElem = $("<p></p>").text("Duration: " + currentDurationHours + ":" + currentDurationMinutes);
+
+            currentInfoDiv.append(currentDateElem);
+            currentIconDiv.append(currentIconElem);
+            currentIconDiv.append(currentDescriptionElem);
+            currentTempDiv.append(currentTempElem);
+            currentTempDiv.append(currentFeelsElem);
+            currentDayTimeDiv.append(currentSunriseElem);
+            currentDayTimeDiv.append(currentSunsetElem);
+            currentDayTimeDiv.append(currentDurationElem);
 
 
-            let weatherDate = new Date(1000 * wDate);
-            let weatherDay = String(weatherDate.getDate());
-            let weatherMonth = String(weatherDate.getMonth() + 1); //January is 0!
-            let weatherYear = weatherDate.getFullYear();
-            let dateElem = $("<h4></h4>").text(weatherDay + '.' + weatherMonth + '.' + weatherYear);
+            let hourlyValue = $(".hourlyValue");
+            let hourlyValueDiv = "";
+            $.each(weatherObj.hourly, function(index, weatherInfo) {
+                if (index <= 6) {
 
-            let iconElem = $(`<img src = "https://openweathermap.org/img/wn/${wIcon}.png"></img>`);
-            let descriptionElem = $("<p></p>").text(wDescription);
-            let tempElem = $("<h1></h1>").html(wTemp + '&deg;' + 'C');
-            let feelsElem = $("<p></p>").text("Real Feel " + wFeels);
+                    hourlyValueDiv = $(`<div class='hourlyValueDiv${index}'></div>`);
+                    $(hourlyValue).append(hourlyValueDiv);
 
-            let sunriseDate = new Date(1000 * wSunrise);
-            let sunriseHours = String(sunriseDate.getHours());
-            let sunriseMinutes = String(sunriseDate.getMinutes());
-            let sunriseElem = $("<p></p>").text("Sunrise: " + sunriseHours + ":" + sunriseMinutes);
+                    let valueDiv = $(`.hourlyValueDiv${index}`);
 
-            let sunsetDate = new Date(1000 * wSunset);
-            let sunsetHours = String(sunsetDate.getHours());
-            let sunsetMinutes = String(sunsetDate.getMinutes());
-            let sunsetElem = $("<p></p>").text("Sunset: " + sunsetHours + ":" + sunsetMinutes);
+                    let hourlyWeatherTime = weatherInfo.dt;
+                    let hourlyDate = new Date(1000 * hourlyWeatherTime);
+                    let hourlyHours = hourlyDate.getHours() < 12 ? (hourlyDate.getHours() + "am") : ((hourlyDate.getHours() - 12) + "pm");
+                    let hourlyTimeElem = $("<p class='hours'></p>").text(hourlyHours);
+                    valueDiv.append(hourlyTimeElem);
 
-            let wDurationHours = sunsetHours - sunriseHours;
-            let wDurationMinutes = sunsetMinutes - sunriseMinutes;
-            let durationElem = $("<p></p>").text("Duration: " + wDurationHours + ":" + wDurationMinutes);
+                    let hourlyIcon = weatherInfo.weather[0].icon;
+                    let hourlyIconElem = $(`<img src = "https://openweathermap.org/img/wn/${hourlyIcon}.png"></img>`);
+                    valueDiv.append(hourlyIconElem);
 
-            infoDiv.append(dateElem);
-            iconDiv.append(iconElem);
-            iconDiv.append(descriptionElem);
-            tempDiv.append(tempElem);
-            tempDiv.append(feelsElem);
-            dayTimeDiv.append(sunriseElem);
-            dayTimeDiv.append(sunsetElem);
-            dayTimeDiv.append(durationElem);
+                    let hourlyDescription = weatherInfo.weather[0].description;
+                    let hourlyDescriptionElem = $("<p></p>").text(hourlyDescription);
+                    valueDiv.append(hourlyDescriptionElem);
+
+                    let hourlyTemp = weatherInfo.temp;
+                    let hourlyTempElem = $("<p></p>").html(hourlyTemp + '&deg;');
+                    valueDiv.append(hourlyTempElem);
+
+                    let hourlyFeels = weatherInfo.feels_like;
+                    let hourlyFeelsElem = $("<p></p>").html(hourlyFeels + '&deg;');
+                    valueDiv.append(hourlyFeelsElem);
+
+                    let windDeg = weatherInfo.wind_deg;
+                    let hourlyWind = (weatherInfo.wind_speed * 3, 6) + " " + toCompass(windDeg);
+                    let hourlyWindElem = $("<p></p>").html(hourlyWind);
+                    valueDiv.append(hourlyWindElem);
+
+                    index++;
+
+                }
+            });
+
+        }
+    }
+    request.send();
+}
+
+
+function openRequest1(lat, lon) {
+    let request;
+    if (window.XMLHttpRequest) {
+        request = new XMLHttpRequest();
+    } else {
+        request = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    let weatherObj1;
+
+    request.open("GET", `https://api.openweathermap.org/data/2.5/find?lat=${lat}&lon=${lon}&cnt=4&units=metric&APPID=a00b1af3da62c683cbdd2b93bb19dc93`);
+
+    request.onload = function() {
+        if (request.status === 200) {
+            weatherObj1 = JSON.parse(request.response);
+            console.log(weatherObj1);
+
+            let cityWeather = $(".cityWeather");
+            let cityWeatherDiv = "";
+            $.each(weatherObj1.list, function(index, cityInfo) {
+
+                cityWeatherDiv = $(`<div class='cityDiv${index} city'></div>`);
+                $(cityWeather).append(cityWeatherDiv);
+                let cityDiv = $(`.cityDiv${index}`);
+
+                let cityName = cityInfo.name;
+                let cityNameElem = $("<p></p>").text(cityName);
+                cityDiv.append(cityNameElem);
+
+                let cityIcon = cityInfo.weather[0].icon;
+                let cityIconElem = $(`<img src = "https://openweathermap.org/img/wn/${cityIcon}.png"></img>`);
+                cityDiv.append(cityIconElem);
+
+                let cityTemp = cityInfo.main.temp;
+                let cityTempElem = $("<p></p>").html(cityTemp + '&deg;' + 'C');
+                cityDiv.append(cityTempElem);
+
+                index++;
+
+
+            });
         }
     }
     request.send();
